4e1fc395b741dddb7ef83d7568547e7d28527c6a
moustafa-natour <moutafa.natour@gmail.com>
2023-12-30 20:59:53 +0200

    v3.3

--- src/modules/components/AppBar.js
+++ src/modules/components/AppBar.js
@@ -2,11 +2,7 @@ import * as React from 'react';
 import MuiAppBar from '@mui/material/AppBar';
 
 function AppBar(props) {
-<<<<<<< HEAD
     return <MuiAppBar elevation={5} position="fixed" {...props} />;
-=======
-    return <MuiAppBar elevation={10} position="fixed" {...props} />;
->>>>>>> 744a9a6eae73bb873af39b799f326272e75154e8
 }
 
 export default AppBar;
