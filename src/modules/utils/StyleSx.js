eb5b9c55935690b5fcab056be437ff6b7c74a884
moustafa-natour <moutafa.natour@gmail.com>
2024-01-19 23:44:30 +0200

    V5 Speedometer

--- src/modules/utils/StyleSx.js
+++ src/modules/utils/StyleSx.js
@@ -20,12 +20,16 @@ export const ContainerSx = {
     justifyContent: 'center'
 };
 export const CardSx = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     transition: 'transform 0.2s ease-in-out',
     '&:hover': {
-        transform: 'scale(1.05)',  // Increase the scale on hover
+        transform: 'scale(1.18)',  // Increase the scale on hover
     },
     '&:focus': {
-        transform: 'scale(1.05)',  // Increase the scale on focus
+        transform: 'scale(1.18)',  // Increase the scale on focus
     },
     cursor: 'pointer',  // Set cursor to pointer on hover
 };
--- src/modules/views/Pricing.js
+++ src/modules/views/Pricing.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Box, Button, Card, CardActions, CardContent, CardHeader, CssBaseline, Grid, Typography, GlobalStyles, Container } from '@mui/material';
 import StarIcon from '@mui/icons-material/StarBorder';
@@ -7,8 +7,15 @@ import AppFooter from './AppFooter';
 import AppAppBar from './AppAppBar';
 import { BoxSx, ContainerSx, TypographyHeaderSx, listItemStyle, TypographyStyle, CardSx } from '../utils/StyleSx';
 import { handleWhatsAppClick } from '../utils/ComonFunc';
-
-
+import Speedometer, {
+    Background,
+    Arc,
+    Needle,
+    Progress,
+    Marks,
+    DangerPath,
+    Indicator,
+} from 'react-speedometer';
 const tiers = [
     {
         title: 'Basic Plan',
@@ -20,6 +27,7 @@ const tiers = [
         dailyLimit: '10 Gigabytes/day',
         buttonText: 'Apply Now',
         buttonVariant: 'outlined',
+        speed: 5,
     },
     {
         title: 'Standard Plan',
@@ -32,6 +40,7 @@ const tiers = [
         dailyLimit: '12 Gigabytes/day',
         buttonText: 'Apply Now',
         buttonVariant: 'outlined',
+        speed: 6,
     },
     {
         title: 'Enhanced Plan',
@@ -43,6 +52,7 @@ const tiers = [
         dailyLimit: '16 Gigabytes/day',
         buttonText: 'Apply Now',
         buttonVariant: 'outlined',
+        speed: 8,
     },
     {
         title: 'Premium Plan',
@@ -54,6 +64,7 @@ const tiers = [
         dailyLimit: '20 Gigabytes/day',
         buttonText: 'Apply Now',
         buttonVariant: 'outlined',
+        speed: 10,
     },
     {
         title: 'Elite Plan',
@@ -65,6 +76,7 @@ const tiers = [
         dailyLimit: '24 Gigabytes/day',
         buttonText: 'Apply Now',
         buttonVariant: 'outlined',
+        speed: 12,
     },
     {
         title: 'Ultimate Plan',
@@ -76,27 +88,61 @@ const tiers = [
         dailyLimit: '40 Gigabytes/day',
         buttonText: 'Apply Now',
         buttonVariant: 'outlined',
+        speed: 20,
     },
 ];
 
-
-
 const defaultTheme = createTheme();
-
-
 const Pricing = () => {
     const [isFullScreenDescriptionOpen, setFullScreenDescriptionOpen] = React.useState(false);
     const [selectedTier, setSelectedTier] = React.useState(null);
+    const [animatedSpeed, setAnimatedSpeed] = useState(0);
+    const [isAnimationComplete, setIsAnimationComplete] = useState(false);
 
     const handleOpenFullScreenDescription = (tier) => {
         setSelectedTier(tier);
         setFullScreenDescriptionOpen(true);
     };
-
     const handleCloseFullScreenDescription = () => {
         setSelectedTier(null);
         setFullScreenDescriptionOpen(false);
     };
+    useEffect(() => {
+        if (!isAnimationComplete) {
+            const animationDuration = 2000; // Adjust the duration as needed
+            const steps = 50; // Number of steps in the animation
+
+            const animateTier = (tier) => {
+                const stepValue = tier.speed / steps;
+
+                let step = 0;
+
+                const interval = setInterval(() => {
+                    setAnimatedSpeed((prev) => ({
+                        ...prev,
+                        [tier.title]: prev[tier.title] + stepValue,
+                    }));
+                    step++;
+
+                    if (step >= steps) {
+                        clearInterval(interval);
+                    }
+                }, animationDuration / steps);
+            };
+
+            tiers.forEach((tier) => {
+                if (!animatedSpeed[tier.title] || animatedSpeed[tier.title] < tier.speed) {
+                    setAnimatedSpeed((prev) => ({
+                        ...prev,
+                        [tier.title]: 0,
+                    }));
+                    animateTier(tier);
+                }
+            });
+
+            setIsAnimationComplete(true);
+        }
+    }, [isAnimationComplete, animatedSpeed, tiers]);
     return (
         <>
             <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
@@ -112,86 +158,147 @@ const Pricing = () => {
                         {'Our pricing table is thoughtfully curated to provide valuable solutions for your organization.Leveraging the power of Internet '}
                         {'We offer a seamless and efficient experience with bundle addons for your convenience.'}
                     </Typography>
-                    <Container maxWidth="md" component="main" sx={{ my: 4, mb: 7 }}>
-                        <Grid container spacing={5} alignItems="flex-end">
-                            {tiers.map((tier) => (
-                                <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
-                                    <Card sx={CardSx}>
-                                        <CardHeader
-                                            onClick={() => handleOpenFullScreenDescription(tier)}
-                                            title={
-                                                <>
-                                                    {tier.title} <SpeedIcon fontSize="large" sx={{ ml: 1, color: 'red' }} />
-                                                </>
-                                            }
-                                            subheader={tier.subheader}
-                                            titleTypographyProps={{ align: 'center' }}
-                                            action={tier.title === 'Pro' ? <StarIcon /> : null}
-                                            subheaderTypographyProps={{ align: 'center' }}
-                                            sx={{
-                                                backgroundColor: (theme) =>
-                                                    theme.palette.mode === 'light'
-                                                        ? theme.palette.grey[200]
-                                                        : theme.palette.grey[700],
-                                            }}
-                                        />
-                                        <CardContent
-                                            onClick={() => handleOpenFullScreenDescription(tier)}>
-                                            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'baseline', mb: 2, pb: 2, pt: 2 }}>
-                                                <Typography component="h5" variant="h5" color="text.primary">
-                                                    {tier.price} L.B.P
-                                                </Typography>
-                                                <Typography variant="h6" color="text.secondary">
-                                                    /mo
-                                                </Typography>
-                                            </Box>
-                                            <ul>
-                                                {tier.description.map((line) => (
-                                                    <Typography component="li" variant="subtitle1" align="center" key={line}>
-                                                        {line}
-                                                    </Typography>
-                                                ))}
-                                            </ul>
-                                        </CardContent>
-                                        <CardActions>
-                                            <Button
-                                                fullWidth
-                                                variant={tier.buttonVariant}
-                                                onClick={() => handleWhatsAppClick(tier)}
-                                            >
-                                                {tier.buttonText}
-                                            </Button>
-                                        </CardActions>
+                    <Grid container spacing={2} alignItems="flex-end">
+                        {tiers.map((tier) => (
+                            <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 12} md={6}>
+                                <Card sx={CardSx}>
+                                    <CardHeader
+                                        onClick={() => handleOpenFullScreenDescription(tier)}
+                                        title={
+                                            <>
+                                                {tier.title}
+                                            </>
+                                        }
+                                        subheader={tier.subheader}
+                                        titleTypographyProps={{ align: 'center' }}
+                                        action={tier.title === 'Pro' ? <StarIcon /> : null}
+                                        subheaderTypographyProps={{ align: 'center' }}
+                                        sx={{
+                                            width: '100%',
+                                            backgroundColor: (theme) =>
+                                                theme.palette.mode === 'light'
+                                                    ? theme.palette.grey[200]
+                                                    : theme.palette.grey[700],
+                                        }}
+                                    />
+                                    <CardContent
+                                        onClick={() => handleOpenFullScreenDescription(tier)}
+                                        sx={{
+                                            flex: 1,
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            justifyContent: 'space-between',
+                                        }}
+                                    >
 
-                                    </Card>
-                                </Grid>
-                            ))}
-                        </Grid>
+                                        <div
+                                            style={{
+                                                background: 'linear-gradient(45deg, #00a0e5, #0090d6, #0077bd, #C13584,#5851DB, #833AB4)',
+                                                padding: '14px',
+                                                borderRadius: '50%',
+                                                width: 'auto', // Adjust the width and height to fit your design
+                                                height: 'auto',
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                            }}
+                                        >
+                                            <Speedometer
+                                                value={animatedSpeed[tier.title] || 0}
+                                                max={20}
+                                                fontFamily='squada-one'
+                                                needleHeightRatio={0.7}
+                                                needleColor="#0084ff"
+                                                startColor="#0084ff"
+                                                endColor="#ff0000"
+                                                currentValueText={tier.speed + 'MB/s'}
+                                                ringWidth={15}
+                                                needleTransitionDuration={3000}
+                                                needleTransition="easeElastic"
+                                                textColor="#333"
+                                                valueFormat="d"
+                                                customSegmentStops={[0, 5, 10, 15, 20]}
+                                                customSegmentLabels={[
+                                                    { text: '0 MB/s', position: 'OUTSIDE' },
+                                                    { text: '5 MB/s', position: 'OUTSIDE' },
+                                                    { text: '10 MB/s', position: 'OUTSIDE' },
+                                                    { text: '15 MB/s', position: 'OUTSIDE' },
+                                                    { text: '20 MB/s', position: 'OUTSIDE' },
+                                                ]}
+                                                marks={{
+                                                    0: '0 MB/s',
+                                                    5: '5 MB/s',
+                                                    10: '10 MB/s',
+                                                    15: '15 MB/s',
+                                                    20: '20 MB/s',
+                                                }}
+                                            >
+                                                <Background outerRadius={90} innerRadius={80} />
+                                                <Arc arcWidth={4} />
+                                                <Needle
+                                                    baseOffset={55}
+                                                    circleRadius={25}
+                                                    needleHeightRatio={0.7}
+                                                    color="cyan"
+                                                    circleColor='#1d5293'
+                                                />
+                                                <DangerPath arcWidth={12} angle={100} color="red" offset={-7} />
+                                                <Progress arcWidth={12} />
+                                                <Marks step={1} />
+                                            </Speedometer>
+                                        </div>
+
+
+
+                                        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mb: 2, pb: 2, pt: 2 }}>
+                                            <Typography component="h5" variant="h5" color="text.primary">
+                                                {tier.price} L.B.P
+                                            </Typography>
+                                            <Typography variant="h6" color="text.secondary">
+                                                /mo
+                                            </Typography>
+                                        </Box>
+                                        <ul>
+                                            {tier.description.map((line) => (
+                                                <Typography component="li" variant="subtitle1" align="center" key={line}>
+                                                    {line}
+                                                </Typography>
+                                            ))}
+                                        </ul>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button
+                                            fullWidth
+                                            variant={tier.buttonVariant}
+                                            onClick={() => handleWhatsAppClick(tier)}
+                                        >
+                                            {tier.buttonText}
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                    <Typography
+                        variant="subtitle2"
+                        align="center"
+                        sx={{ textAlign: 'center', fontSize: { xs: 12, md: 20 }, my: 4 }}
+                    >
+                        {'All profiles are subject to our'}
                         <Typography
                             variant="subtitle2"
-                            align="center"
-                            sx={{ textAlign: 'center', fontSize: { xs: 12, md: 20 }, my: 4 }}
+                            onClick={() => { window.location.href = "/fairusepolicy" }}
+                            sx={{
+                                ...listItemStyle, textAlign: 'center', fontSize: { xs: 12, md: 20 },
+                                display: 'inline', // or 'display: inline-block'
+                                marginLeft: 1, // Adjust the spacing between the two Typography components
+                            }}
                         >
-                            {'All profiles are subject to our'}
-
-                            <Typography
-                                variant="subtitle2"
-                                onClick={() => { window.location.href = "/fairusepolicy" }}
-                                sx={{
-                                    ...listItemStyle, textAlign: 'center', fontSize: { xs: 12, md: 20 },
-                                    display: 'inline', // or 'display: inline-block'
-                                    marginLeft: 1, // Adjust the spacing between the two Typography components
-                                }}
-                            >
-                                {'Fair Use Policy '}
-                            </Typography>
+                            {'Fair Use Policy '}
                         </Typography>
-
-                    </Container>
+                    </Typography>
                 </Box>
             </Container >
-
-
             {/* Render Full-Screen Description */}
             {selectedTier && (
                 <FullScreenDescription
@@ -200,7 +307,6 @@ const Pricing = () => {
                     tier={selectedTier}
                 />
             )}
-
             <AppFooter />
         </>
     );
